539ada6b997a9ede908a5ead2074bb46620e59d3
Kevin Chant <kchant1975@hotmail.com>
2025-05-19 14:01:05 +0200

    Reverted change

--- scripts/generate_forks_md.py
+++ scripts/generate_forks_md.py
@@ -24,7 +24,6 @@ with open("forks.md", "w", encoding="utf-8") as f:
             name = repo["name"]
             desc = repo.get("description") or ""
             url = repo["html_url"]
-            # source_url = repo.get("source", {}).get("html_url", "Unknown source URL")
             f.write(f"- [{name}]({url}) - {desc}\n")
     else:
         f.write("No forked repositories found.\n")
